d8fecf45a60f67c356fbaf417b44981493136960
titoreal <titintr@hotmail.com>
2024-08-15 13:19:00 -0500

    Eliminando todos los archivos rastreados

--- app/src/main/java/com/titin/pendientes/domain/use_case/AllUseCases.kt
+++
@@ -1,9 +0,0 @@
-package com.titin.pendientes.domain.use_case
-
-data class AllUseCases(
-    val deleteToDoUseCase: DeleteToDoUseCase,
-    val getAllToDoUseCase: GetAllToDoUseCase,
-    val getSingleToDoUseCase: GetSingleToDoUseCase,
-    val getToDoByPriorityUseCase: GetToDoByPriorityUseCase,
-    val insertOrUpdateToDoUseCase: InsertOrUpdateToDoUseCase
-)
